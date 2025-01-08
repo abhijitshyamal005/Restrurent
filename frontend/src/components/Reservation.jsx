bfb4cabaf5c6b00d3139a2882c985f2a9225d89e
ABHIJIT SHYAMAL <abhijitshyamal005@gmail.com>
2025-01-08 22:10:53 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -15,7 +15,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8000/api/v1/reservation/send",
+        "https://restrurent.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
